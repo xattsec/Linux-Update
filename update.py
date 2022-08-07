fa72029aedfbd9f800b729f20266524267f9be47
XATT <106667154+xattsec@users.noreply.github.com>
2022-08-07 14:14:27 +0200

    Add files via upload

---
+++ update.py
@@ -0,0 +1,23 @@
+import os
+import platform
+
+update = input("Would you like to update your machine?:\n")
+if update.lower() == "yes":
+    x = ['ubuntu', 'mint', 'kdeneon', 'deepin', 'kali', 'debian', 'nitrux', 'kubuntu', 'zorinos', 'tails']
+    v = "arch"
+    z = "fedora"
+    print("Checking distro...")
+    if v in platform.platform():
+        os.system('sudo pacman -Syu')
+        os.system('yay -Syu')
+        print("Done updating.")
+    elif x in platform.platform():
+        os.system('sudo apt update && sudo apt upgrade -y')
+        print("Done updating.")
+    elif v in platform.platform():
+        os.system('sudo dnf update && sudo dnf upgrade -y')
+        print("Done updating.")
+    else:
+        print("Could not find OS in selection.")
+else:
+    exit()
